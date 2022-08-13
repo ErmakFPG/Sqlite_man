7255fce7968fe2cf2ee7e7c2e2abdd1d82527956
ErmakFPG <dm.eremin271090@gmail.com>
2022-08-13 12:04:48 +0300

    some fix

--- sqlite_man/mapping/models.py
+++ sqlite_man/mapping/models.py
@@ -5,3 +5,6 @@ class Mapping(models.Model):
     name = models.CharField(max_length=20)
     key = models.CharField(max_length=20)
     value = models.CharField(max_length=20)
+
+    class Meta:
+        unique_together = ['name', 'key']
